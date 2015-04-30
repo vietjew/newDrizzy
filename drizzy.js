395bfbe9d955aa92546e1d721b37118a416feaa0
Joseph Nguyen <vietjew@gmail.com>
2015-04-30 12:56:39 -0700

    Minor updates

--- drizzy.js
+++ drizzy.js
@@ -2,7 +2,9 @@ console.log("New Drizzy is running!");
 var Twit = require('twit');
 var express = require('express');
 var app = express();
-
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 var port = process.env.PORT || 3000;
 app.listen(port);
 
